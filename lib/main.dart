f8ff895b91657683a8b51b35e033ccb9eb1e00ef
Kunsh <kunshmittal15@gmail.com>
2025-04-19 13:41:10 +0530

    fix: correct typo in car details page file name
    The file `car_detals_page.dart` was renamed to `car_details_page.dart` to fix a typo in the file name. This ensures consistency and avoids potential issues with file references in the codebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:car_rental/presentation/pages/car_detals_page.dart';
+import 'package:car_rental/presentation/pages/car_details_page.dart';
 import 'package:car_rental/presentation/pages/car_list_screen.dart';
 import 'package:car_rental/presentation/pages/onboarding_page.dart';
 import 'package:flutter/material.dart';
